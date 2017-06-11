5005ad84933e46ff7d3ebc7a09631bb547684186
Syntaf <gmercer015@gmail.com>
2017-06-10 17:46:08 -0700

    configure map style and zoom/center

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,12 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3ludGFmIiwiYSI6ImNqM2Z2bzZhbTAxZWwycW4wcmI5cjk4MW0ifQ.YOd5yuJfLARC2oOfqY-KoA'
+window.onload = function() {
+    var map = new mapboxgl.Map({
+        center: [0, 35],
+        zoom: 1.75,
+        container: 'map',
+        style: 'mapbox://styles/syntaf/cj3rys2fx000q2qodb7zlbh2v'
+    });
+};
