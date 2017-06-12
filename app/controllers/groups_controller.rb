b00fe4f2abf42fc76630ac9fd1ed259ab95ebb86
Syntaf <gmercer015@gmail.com>
2017-06-11 19:18:34 -0700

    work on administrative panel

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,51 @@
+require "json"
+
+class GroupsController < ApplicationController
+    def new
+        @group = Group.new
+    end
+
+    def edit
+        @group = Group.find(params[:id])
+    end
+
+    def create
+        @group = Group.new(group_params)
+
+        if @group.save
+            redirect_to @group
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @group = Group.find(params[:id])
+
+        if @group.update(group_params)
+            redirect_to @group
+        else
+            render 'edit'
+        end
+    end
+
+    def show
+        @group = Group.find(params[:id])
+    end
+
+    def index
+        @groups = Group.all
+    end
+
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+
+        redirect_to groups_path
+    end
+
+    private
+        def group_params
+            params.require(:group).permit(:name, :members, cords: [] )
+        end
+end
--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AdminControllerTest < ActionDispatch::IntegrationTest
+class GroupsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
