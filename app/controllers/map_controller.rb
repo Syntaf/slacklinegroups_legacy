bf79e6b6d6157c702bfcd04202f4996dd078fd01
Syntaf <gmercer015@gmail.com>
2017-06-14 14:54:20 -0700

    establish connection with database

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,30 @@
+require 'json'
+
 class MapController < ApplicationController
     def index
+    end
+
+    def groups
+        @groups = Group.all
+        @geojson = Array.new
+
+        @groups.each do |group|
+            @geojson << {
+                type: 'Feature',
+                geometry: {
+                    type: 'Polygon',
+                    coordinates: group.cords
+                },
+                properties: {
+                    name: group.name,
+                    members: group.members
+                }
+            }
+        end
+
+        respond_to do |format|
+            format.html
+            format.json { render :json => @geojson }
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'map/index'
+  get 'map/groups'
 
   resources :groups
 
