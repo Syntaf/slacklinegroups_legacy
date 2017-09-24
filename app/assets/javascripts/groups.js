8d121fa46e8041b726bb9a276cac4f9fd5e05a98
Syntaf <gmercer015@gmail.com>
2017-09-23 18:29:24 -0700

    Finish submit new form validation

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,3 +1,8 @@
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 $(document).ready(function() {
     $('#url-type').change(function() {
         switch ($('#url-type').find(':selected').val()) {
@@ -24,65 +29,71 @@ $(document).ready(function() {
         var form = $('#new_user_submitted_group');
         var groupName = $('#user_submitted_group_name');
         var error = $('#error_explanation');
+        var groupType = $('#url-type').find(":selected").text();
+        console.log(groupType);
+
         if (groupName.val().trim().length <= 0) {
             e.preventDefault();
             $('#error-msg').remove();
-            form.before('<p id="error-msg" style="color: red; text-align: center; width: 100%;">Please provide a group type</p>');
             groupName.addClass('error');
-            $('.submit-button').prop('disabled', false);
             return;
         }
 
-        var groupType = $('#url-type');
+        groupName.removeClass('error');
         
-        if (groupType.val().trim().length <= 0) {
+        if (groupType === 'Group Type') {
             e.preventDefault();
             $('#error-msg').remove();
-            form.before('<p id="error-msg" style="color: red; text-align: center; width: 100%;">Please provide a group type</p>');
-            groupType.addClass('error');
+            $('.select-dropdown').addClass('error');
             return;
         }
 
+        $('.select-dropdown').removeClass('error')
+        
         switch (groupType) {
-            case 'group':
+            case 'Facebook Group':
                 var groupGroupUrl = $('#user_submitted_group_fb_group');
                 
                 if (groupGroupUrl.val().trim().length <= 0 ) {
                     e.preventDefault();
-                    error.val('Please provide a group link');
                     groupGroupUrl.addClass('error');
                     return;
                 }
+
+                groupGroupUrl.removeClass('error');
             break;
-            case 'page':
+            case 'Facebook Page':
                 var groupPageUrl = $('#user_submitted_group_fb_page');
                 
                 if (groupPageUrl.val().trim().length <= 0) {
                     e.preventDefault();
-                    error.val('Please provide a group link');
                     groupPageUrl.addClass('error');
                     return;
                 }
+
+                groupPageUrl.removeClass('error');
             break;
-            case 'other':
+            case 'Website/Other':
                 var groupOtherUrl = $('#user_submitted_group_website');
             
                 if (groupOtherUrl.val().trim().length <= 0) {
                     e.preventDefault();
-                    error.val('Please provide a group link');
                     groupOtherUrl.addClass('error');
                     return;
                 }
+
+                groupOtherUrl.removeClass('error');
             break;
         }
 
-        var groupEmail = $('#user_submitted_group_email')();
+        var groupEmail = $('#user_submitted_group_email');
 
-        if (groupType.val.trim().length <= 0) {
+        if (groupEmail.val().trim().length <= 0 || !validateEmail(groupEmail.val())) {
             e.preventDefault();
-            error.val('Please provide a contact email');
-            groupType.addClass('error');
+            groupEmail.addClass('error');
             return;
         }
+
+        groupEmail.removeClass('error');
     });
 });
