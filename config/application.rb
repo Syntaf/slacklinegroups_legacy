6c3516eba21453e6a09cd0621fe5cd2d610e1e0e
Syntaf <gmercer015@gmail.com>
2017-11-16 21:40:49 -0800

    Update seeds before large migration to new db structure

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Slacklinegroups
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Allow slacklinegroups to be placed within an iframe
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'SAMEORIGIN'
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
